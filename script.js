9dfbdc86aaf6e10718f702d774adf0fc205ad4b4
sticktostack <rajdeep6153@gmail.com>
2024-02-29 15:13:25 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let searhForm = document.getElementById("Searchform");
-let header = document.querySelector('.header')
+let header = document.querySelector(".header");
 let SearchArea = document.getElementById("Searcharea");
 let searchBtn = document.getElementById("searchbutton");
 let imageContainer = document.getElementById("imagecontainer");
@@ -8,11 +8,15 @@ let accesKey = "Pbh1B2iN-Np8bXsoy84IST4lMcx5olIzJ4DEADy_718";
 let keyword = "";
 let page = 1;
 
+let orientations = document.getElementById("orientations");
+let potrait = document.getElementById("potrait");
+let landscape = document.getElementById("landscape");
+
 // searching images based on keywords
 function searchImage() {
   keyword = SearchArea.value;
-  fetch(
-    `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&orientation=landscape&client_id=${accesKey}&per_page=6`
+    fetch(
+    `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesKey}&per_page=6`
   )
     .then((response) => {
       return response.json();
@@ -23,13 +27,13 @@ function searchImage() {
         imageContainer.innerHTML = "";
       }
       if (results.length !== 0) {
-        header.innerHTML = `Results related ${keyword}`
+        header.innerHTML = `Results related ${keyword}`;
         loadmoreBtn.style.display = "block";
       } else {
         imageContainer.innerHTML = "No images found";
       }
-      if(SearchArea.value === ''){
-        header.innerHTML = `<h1><span>Search</span> any <span>image</span></h1>`
+      if (SearchArea.value === "") {
+        header.innerHTML = `<h1><span>Search</span> any <span>image</span></h1>`;
       }
       results.forEach((result) => {
         let image = document.createElement("img");
@@ -38,7 +42,7 @@ function searchImage() {
         imageContainer.appendChild(image);
       });
 
-      console.log(results);
+      // console.log(results);
     });
 }
 
@@ -55,9 +59,75 @@ searchBtn.addEventListener("click", () => {
 loadmoreBtn.addEventListener("click", () => {
   page++;
   searchImage();
-  // console.log(data);
+  landscapeImage()
 });
 
-let orientations = document.getElementById('orientations')
-let potrait = document.getElementById('potrait')
-let landscape = document.getElementById('landscape')
+// fetching landscape images 
+function landscapeImage(){
+  keyword = SearchArea.value; 
+  fetch(`https://api.unsplash.com/search/photos?page=${page}&orientation=landscape&query=${keyword}&client_id=${accesKey}&per_page=6`)
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    let results = data.results;
+    if (page === 1) {
+      imageContainer.innerHTML = "";
+    }
+    if (results.length !== 0) {
+      header.innerHTML = `Results related ${keyword}`;
+      loadmoreBtn.style.display = "block";
+    } else {
+      imageContainer.innerHTML = "No images found";
+    }
+    results.forEach((result) => {
+      let image = document.createElement("img");
+      image.src = result.urls.small;
+      image.id = "images";
+      imageContainer.appendChild(image);
+    });
+
+    console.log(data);
+  })
+}
+// fetching potrait images 
+function potraitImage(){
+  keyword = SearchArea.value; 
+  fetch(`https://api.unsplash.com/search/photos?page=${page}&orientation=portrait&query=${keyword}&client_id=${accesKey}&per_page=6`)
+  .then((res)=>{
+    return res.json()
+  })
+  .then((data)=>{
+    let results = data.results;
+    if (page === 1) {
+      imageContainer.innerHTML = "";
+    }
+    if (results.length !== 0) {
+      header.innerHTML = `Results related ${keyword}`;
+      loadmoreBtn.style.display = "block";
+    } else {
+      imageContainer.innerHTML = "No images found";
+    }
+    results.forEach((result) => {
+      let image = document.createElement("img");
+      image.src = result.urls.small;
+      image.id = "images";
+      imageContainer.appendChild(image);
+    });
+
+    console.log(data);
+  })
+}
+
+
+// event on select field
+orientations.addEventListener("change", () => {
+  if(orientations.options[orientations.selectedIndex] === landscape){
+    landscapeImage()
+  }else{
+   potraitImage()
+  }
+  // console.log(orientations.options[orientations.selectedIndex]); 
+});
+
+
